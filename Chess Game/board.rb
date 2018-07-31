e016a10716c25a11320d87a1620e6828b1162bf6
Justin <misterjustincox@gmail.com>
2018-07-31 16:24:20 -0400

    Initialize board with buggy move_piece

--- Chess Game/board.rb
+++ Chess Game/board.rb
@@ -2,23 +2,36 @@ require_relative "piece"
 
 class Board
 
+  attr_accessor :board
+
   def initialize
     @board = Array.new(8) {Array.new(8){NullPiece.new}}
-
+    @board[0] = Array.new(8){Piece.new}
+    @board[1] = Array.new(8){Piece.new}
+    @board[6] = Array.new(8){Piece.new}
+    @board[7] = Array.new(8){Piece.new}
   end
 
-  def move_piece(color, start_pos, end_pos)
+  def [](pos)
+    x, y = pos
+    @board[x][y]
+  end
+
+  def []=(pos, piece)
+    x, y = pos
+    @board[x][y] = piece
+  end
+
+  def move_piece(start_pos, end_pos)
     if @board[start_pos].class == NullPiece
       raise "There is no piece there, you fool!"
-    elsif Piece.color
-      # if the piece at the end position is the same color as your piece, this is not a valid move :(
-
-    end
-
-    if valid_pos?(end_pos)
+    elsif @board[end_pos].nil? || @board[start_pos].nil?
+      # if the piece at the end position is the same color as your piece,
+      # this is not a valid move :(
+      raise "That is not a position on the board!"
+    else
+      @board[end_pos] = @board[start_pos]
       @board[start_pos] = NullPiece.new
-      @board[end_pos] = nil #piece in question
-      #execute the rest
     end
   end
 
--- Chess Game/piece.rb
+++ Chess Game/piece.rb
@@ -1,6 +1,8 @@
 class Piece
 
-
+  def initialize(type)
+    @type = type
+  end
 end
 
 class NullPiece < Piece
